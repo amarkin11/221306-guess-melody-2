4dbcd01e96acb137834d8e4d59c93835ab89a99a
AMarkin <chudokreker@ya.ru>
2019-10-26 12:54:40 +0300

    fixes import app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from './components/app/app';
+import App from './components/app/app.jsx';
 
 const init = () => {
   ReactDOM.render(
